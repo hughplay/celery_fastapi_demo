b6a8159eb333abf72bd32944efdfe165ab74bfcd
Mr.Blue <silverhugh.77@gmail.com>
2024-01-26 16:01:41 +0800

    add api for grouping tasks

--- api/api.py
+++ api/api.py
@@ -1,25 +1,33 @@
 from fastapi import FastAPI
-from celery.result import AsyncResult
-from worker import celery
+from celery.result import AsyncResult, GroupResult
+from celery import group, signature
+from worker import celery_client
 
 app = FastAPI()
 
 
+# ----------------------------
+# Single task
+# ----------------------------
+
+
 @app.post("/add")
 async def add_task(x: int, y: int):
-    task = celery.send_task("add", args=[x, y], queue="queue_add")
+    task = celery_client.send_task("add", args=[x, y], queue="queue_add")
     return {"task_id": task.id}
 
 
 @app.post("/multiply")
 async def multiply_task(x: int, y: int):
-    task = celery.send_task("multiply", args=[x, y], queue="queue_multiply")
+    task = celery_client.send_task(
+        "multiply", args=[x, y], queue="queue_multiply"
+    )
     return {"task_id": task.id}
 
 
 @app.get("/check/{task_id}")
 async def get_task_result(task_id: str):
-    task_result = AsyncResult(task_id, app=celery)
+    task_result = AsyncResult(task_id, app=celery_client)
     if task_result.ready():
         return {
             "task_id": task_id,
@@ -30,5 +38,37 @@ async def get_task_result(task_id: str):
         return {
             "task_id": task_id,
             "status": task_result.status,
+            "result": "Not ready",
+        }
+
+
+# ----------------------------
+# Group task
+# ----------------------------
+
+
+@app.post("/multiply_and_add")
+async def multiply_and_add_task(x: int, y: int):
+    group_task = group(
+        signature("multiply", args=[x, y], queue="queue_multiply"),
+        signature("add", args=[x, y], queue="queue_add"),
+    )()
+    group_task.save()
+    return {"group_task_id": group_task.id}
+
+
+@app.get("/check_group/{group_task_id}")
+async def get_group_task_result(group_task_id: str):
+    task_result = GroupResult.restore(group_task_id, app=celery_client)
+    if task_result.ready():
+        return {
+            "group_task_id": group_task_id,
+            "status": [task.status for task in task_result],
+            "result": task_result.get(),
+        }
+    else:
+        return {
+            "group_task_id": group_task_id,
+            "status": [task.status for task in task_result],
             "result": "Not ready",
         }
--- api/worker.py
+++ api/worker.py
@@ -1,12 +1,14 @@
 import os
 from celery import Celery
 
-CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "rpc://")
+CELERY_RESULT_BACKEND = os.environ.get(
+    "CELERY_RESULT_BACKEND", "redis://localhost"
+)
 CELERY_BROKER_URL = (
     os.environ.get("CELERY_BROKER_URL", "pyamqp://guest:guest@localhost:5672/"),
 )
 
-celery = Celery(
+celery_client = Celery(
     __name__,
     broker=CELERY_BROKER_URL,
     backend=CELERY_RESULT_BACKEND,
